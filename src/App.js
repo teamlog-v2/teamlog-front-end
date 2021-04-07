67006cce756461739301f7da5e982b26c10df857
Hyun-ees <migu554@naver.com>
2021-04-08 01:39:31 +0900

    [추가] 포스트 등록 폼에 필요한 컴포넌트 배치

--- src/App.js
+++ src/App.js
@@ -1,22 +1,128 @@
+import React, { useState, useRef } from 'react';
+import { Grid, Paper, Chip, Button, TextField } from '@material-ui/core';
+import { Lock, LockOpen, Create } from '@material-ui/icons';
 
-function App() {
+const Hashtags = ({ hashtags }) =>
+  hashtags.map((item, index) => <Chip key={index} label={item} />);
+
+const App = () => {
+  const [isLockOpen, setIsLockOpen] = useState(1);
+  const [location, setLocation] = useState('');
+  const contentRef = useRef(null);
+  const [hashtags, setHashtags] = useState([]);
+  const [recommendedHashtags, setRecommendedHashtags] = useState([
+    {
+      key: '1',
+      name: '스토리보드',
+    },
+    {
+      key: '2',
+      name: '일본',
+    },
+  ]);
+  // 미디어
+  const submitPost = () => {
+    console.log({
+      isLockOpen,
+      location,
+      content: contentRef.current.value,
+      hashtags,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container item direction="column" spacing={3} sm={7}>
+      <Grid container item direction="row" justify="space-between">
+        <Grid container item sm={3}>
+          <Paper
+            onClick={() => {
+              setLocation('도쿄 디즈니랜드');
+            }}
+          >
+            🛬좌표를 찍어주세요~
+          </Paper>
+        </Grid>
+        <Grid container item sm={2}>
+          {isLockOpen ? (
+            <Paper
+              elevation={0}
+              onClick={() => {
+                setIsLockOpen(!isLockOpen);
+              }}
+            >
+              내부 멤버만
+              <Lock />
+            </Paper>
+          ) : (
+            <Paper
+              elevation={0}
+              onClick={() => {
+                setIsLockOpen(!isLockOpen);
+              }}
+            >
+              외부에도 공개
+              <LockOpen />
+            </Paper>
+          )}
+        </Grid>
+      </Grid>
+      <Grid container item>
+        <TextField
+          variant="outlined"
+          rows={10}
+          rowsMax={Infinity}
+          multiline
+          fullWidth
+          inputRef={contentRef}
+        />
+      </Grid>
+      <Grid container item>
+        <Grid container item direction="row" justify="space-between">
+          <Grid container item spacing={2}>
+            <Grid container item>
+              <Paper
+                onClick={() => {
+                  setHashtags(['도쿄', '디즈니랜드', '스토리보드']);
+                }}
+              >
+                <Hashtags hashtags={hashtags} />
+              </Paper>
+              <TextField
+                onKeyPress={(event) => {
+                  const { charCode, target } = event;
+                  if (charCode === 13) {
+                    const newHashtags = [...hashtags];
+                    newHashtags.push(target.value);
+                    console.log(newHashtags);
+                    setHashtags(newHashtags);
+                  }
+                }}
+              />
+            </Grid>
+            <Grid container item>
+              <Paper>
+                이런 해시태그는 어때요?
+                {recommendedHashtags.map((item) => (
+                  <Chip key={item.key} label={item.name} />
+                ))}
+              </Paper>
+            </Grid>
+          </Grid>
+          <Grid container item>
+            <Grid container justify="flex-end">
+              <Button
+                onClick={() => {
+                  submitPost();
+                }}
+              >
+                <Create />
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default App;
