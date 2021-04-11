4c4841c094b0ee32e5cb88e10f76bda489f7ed0b
Hyun-ees <migu554@naver.com>
2021-04-11 13:30:00 +0900

    [수정] 포스트 작성 폼
    - google 장소 검색 api 적용
    - isLockOpen true => 내부 멤버 공개로 변경
    - 해시태그 중복 여부 검사 기능 추
    - 작성 버튼 IconButton 컴포넌트 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React, { useState, useRef } from 'react';
-import { Grid, Paper, Chip, Button, TextField } from '@material-ui/core';
-import { Lock, LockOpen, Create } from '@material-ui/icons';
+import {
+  Grid,
+  Chip,
+  TextField,
+  Dialog,
+  Typography
+} from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import { Lock, LockOpen, Create, LocationOn } from '@material-ui/icons';
+import PlacesSearch from './PlacesSearch';
+import { isDuplicateData } from './utils';
 
-const Hashtags = ({ hashtags }) =>
-  hashtags.map((item, index) => <Chip key={index} label={item} />);
+const SimpleDialog = (props) => {
+  const { onClose, open, updateLocation } = props;
+
+  return (
+    <Dialog onClose={onClose} aria-labelledby="simple-dialog-title" open={open}>
+      {/* <DialogTitle id="simple-dialog-title">위치 선택 API</DialogTitle> */}
+      <PlacesSearch updateLocation={updateLocation} onClose={onClose}/>
+    </Dialog>
+  );
+};
+
+const Hashtags = ({ hashtags, updateHashtags }) => {
+  const onDeleteItem = (index) => {
+    const newHashtags = hashtags.filter((item, i) => index !== i);
+    updateHashtags(newHashtags);
+  }
+
+  return hashtags.map((item, index) => <Chip key={index} label={item} onDelete={() => {
+    onDeleteItem(index);
+  }}/>);
+};
 
 const App = () => {
-  const [isLockOpen, setIsLockOpen] = useState(1);
+  const [isLockOpen, setIsLockOpen] = useState(false);
+  const [open, setOpen] = useState(false);
   const [location, setLocation] = useState('');
   const contentRef = useRef(null);
   const [hashtags, setHashtags] = useState([]);
@@ -21,7 +50,31 @@ const App = () => {
     },
   ]);
   // 미디어
+
+  const handleClickOpen = () => {
+    setOpen(1);
+  };
+
+  const handleClose = () => {
+    setOpen(0);
+  };
+
+  const handleHashtagInput = (event) => {
+    const { charCode, target } = event;
+    if (charCode === 13) {
+      const newHashtags = [...hashtags];
+      if (isDuplicateData(hashtags, target.value)) {
+        alert('이미 존재하는 태그입니다!');
+      } else {
+        newHashtags.push(target.value);
+        setHashtags(newHashtags);
+      }
+      target.value = '';
+    }
+  };
+
   const submitPost = () => {
+    alert('작성완료!');
     console.log({
       isLockOpen,
       location,
@@ -33,38 +86,50 @@ const App = () => {
   return (
     <Grid container item direction="column" spacing={3} sm={7}>
       <Grid container item direction="row" justify="space-between">
-        <Grid container item sm={3}>
-          <Paper
-            onClick={() => {
-              setLocation('도쿄 디즈니랜드');
-            }}
-          >
-            🛬좌표를 찍어주세요~
-          </Paper>
+        <Grid container item sm={5}>
+          <Grid onClick={handleClickOpen} style={{ cursor: 'pointer' }}>
+            <LocationOn />
+            <Typography variant='span'>어디를 방문하셨나요?</Typography>
+          </Grid>
+          <TextField variant="outlined" value={location}/>
+          <SimpleDialog
+            open={open}
+            onClose={handleClose}
+            updateLocation={setLocation}
+          />
         </Grid>
-        <Grid container item sm={2}>
-          {isLockOpen ? (
-            <Paper
-              elevation={0}
+        <Grid container item justify='flex-end'>
+          {!isLockOpen ? (
+            <Grid item
               onClick={() => {
                 setIsLockOpen(!isLockOpen);
               }}
+              style={{ cursor: 'pointer' }}
             >
-              내부 멤버만
+              <Typography variant='span'>내부 멤버만</Typography>
               <Lock />
-            </Paper>
+            </Grid>
           ) : (
-            <Paper
-              elevation={0}
+            <Grid item
               onClick={() => {
                 setIsLockOpen(!isLockOpen);
               }}
+              style={{ cursor: 'pointer' }}
             >
-              외부에도 공개
+              <Typography variant='span'>외부에도 공개</Typography>
               <LockOpen />
-            </Paper>
+            </Grid>
           )}
         </Grid>
+      </Grid>
+      <Grid item>
+        <label for="upload"
+        style={{ backgroundColor: 'black', color: 'white',
+        padding: '1%', cursor: 'pointer' }}>
+          사진/동영상/첨부파일
+        </label>
+        <input id="upload" type="file" multiple
+          style={{ display: 'none' }}/>
       </Grid>
       <Grid container item>
         <TextField
@@ -80,43 +145,45 @@ const App = () => {
         <Grid container item direction="row" justify="space-between">
           <Grid container item spacing={2}>
             <Grid container item>
-              <Paper
+              <Grid
                 onClick={() => {
                   setHashtags(['도쿄', '디즈니랜드', '스토리보드']);
                 }}
               >
-                <Hashtags hashtags={hashtags} />
-              </Paper>
+                <Hashtags hashtags={hashtags} updateHashtags={setHashtags}/>
+              </Grid>
               <TextField
-                onKeyPress={(event) => {
-                  const { charCode, target } = event;
-                  if (charCode === 13) {
-                    const newHashtags = [...hashtags];
-                    newHashtags.push(target.value);
-                    console.log(newHashtags);
-                    setHashtags(newHashtags);
-                  }
-                }}
+                onKeyPress={handleHashtagInput}
               />
             </Grid>
             <Grid container item>
-              <Paper>
+              <Grid>
                 이런 해시태그는 어때요?
                 {recommendedHashtags.map((item) => (
-                  <Chip key={item.key} label={item.name} />
+                  <Chip
+                    key={item.key}
+                    label={item.name}
+                    onClick={() => {
+                      const newHashtags = [...hashtags];
+                      if(isDuplicateData(hashtags, item.name)){
+                        alert('이미 존재하는 태그입니다!');
+                        return;
+                      }
+                      newHashtags.push(item.name);
+                      setHashtags(newHashtags);
+                    }}
+                  />
                 ))}
-              </Paper>
+              </Grid>
             </Grid>
           </Grid>
           <Grid container item>
             <Grid container justify="flex-end">
-              <Button
-                onClick={() => {
+              <IconButton onClick={() => {
                   submitPost();
-                }}
-              >
-                <Create />
-              </Button>
+                }} style={{ backgroundColor: 'black' }}>
+                <Create style={{ color: 'white' }}/>
+              </IconButton>
             </Grid>
           </Grid>
         </Grid>
