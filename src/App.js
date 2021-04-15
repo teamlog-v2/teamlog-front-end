7990974b038d0d5a6bd8fc14e170aa999813696d
hamchu <64580070+hamchu@users.noreply.github.com>
2021-04-15 22:31:37 +0900

    [추가] 프로젝트 리스트 조회 컴포넌트
    - ProjectItem, ProjectListContainer 추가
    - @material-ui/lab 패키지 설치 (for Skeleton)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
 import loadable from '@loadable/component';
+import ProjectListContainer from './/project/ProjectListContainer';
+
 const  TaskContainer = loadable(() => import('./task/TaskContainer'));
 
 const App = () => {
   return (
     <Switch>
-      <Redirect exact path="/" to="/task" />
+      <Redirect exact path="/" to="/project" />
       <Route path="/task" component={TaskContainer} />
+      <Route path="/project" component={ProjectListContainer}/>
     </Switch>
   )
 }
