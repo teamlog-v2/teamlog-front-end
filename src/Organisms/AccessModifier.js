85ff2335a28aa71a8205a6d08a316046264ba9c9
Hyun-ees <migu554@naver.com>
2021-04-18 23:13:21 +0900

    [수정] 아이콘, 타이포그래피 정렬

--- src/Organisms/AccessModifier.js
+++ src/Organisms/AccessModifier.js
@@ -7,29 +7,43 @@ import { Lock, LockOpen } from '@material-ui/icons';
 const AccessModifier = ({ isLockOpen,  updateLockOpen}) => {
   return (
   <Grid container item justify='flex-end'>
-  {!isLockOpen ? (
-    <Grid item
-      onClick={() => {
-        updateLockOpen(!isLockOpen);
-      }}
-      style={{ cursor: 'pointer' }}
-    >
-      <Typography>
-      내부 멤버만<Lock />
-      </Typography>
-    </Grid>
-  ) : (
-    <Grid item
-      onClick={() => {
-        updateLockOpen(!isLockOpen);
-      }}
-      style={{ cursor: 'pointer' }}
-    >
-      <Typography>
-        외부에도 공개<LockOpen />
-      </Typography>
-    </Grid>
-  )}
+    {!isLockOpen ? (
+        <Grid item
+          onClick={() => {
+            updateLockOpen(!isLockOpen);
+          }}
+          style={{ cursor: 'pointer' }}
+        >
+          <Grid container alignItems="center">
+            <Grid item>
+              <Lock />
+            </Grid>
+            <Grid item>
+              <Typography>
+              내부 멤버만
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      ) : (
+          <Grid item
+            onClick={() => {
+              updateLockOpen(!isLockOpen);
+            }}
+            style={{ cursor: 'pointer' }}
+          >
+            <Grid container alignItems="center">
+              <Grid item>
+                <LockOpen />
+              </Grid>
+              <Grid item>
+                <Typography>
+                  외부에도 공개
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+    )}
 </Grid>);
 };
 
