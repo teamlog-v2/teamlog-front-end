f5eef169334b7ac8527e3dc67bd3ca91dde0c7e7
Hyun-ees <migu554@naver.com>
2021-04-18 23:20:11 +0900

    [수정] 해시태그 관련 컴포넌트
    - 이벤트 핸들러 함수명 handle_ 로 수정
    - 네이버 해시태그 입력 기능 벤치마킹

--- src/Organisms/HashtagInput.js
+++ src/Organisms/HashtagInput.js
@@ -2,9 +2,21 @@ import React from 'react';
 import {
   Grid,
   Chip,
-  TextField
 } from '@material-ui/core';
 import { isDuplicateData } from '../utils';
+import styled from 'styled-components';
+
+const StyledInput = styled.input`
+  font-size: smaller;
+  border-style: none;
+  color: white;
+  background-color: #C16AF5;
+  border-radius: 500px;
+  padding: 5%;
+  &:focus{
+    outline: none;
+  }
+`;
 
 const Hashtags = ({ hashtags, updateHashtags }) => {
   const onDeleteItem = (index) => {
@@ -12,30 +24,51 @@ const Hashtags = ({ hashtags, updateHashtags }) => {
     updateHashtags(newHashtags);
   }
 
-  return hashtags.map((item, index) => <Chip key={index} label={item} onDelete={() => {
-    onDeleteItem(index);
-  }}/>);
+  return (<Grid container direction='row' spacing={1}>
+          {hashtags.map((item, index) => (<Grid item>
+            <Chip key={index} label={`#${item}`} onDelete={() => {
+              onDeleteItem(index);}} color='primary'/>
+            </Grid>))}
+          </Grid>);
 };
 
 const HashtagInput = ({ hashtags, updateHashtags }) => {
   const handleHashtagInput = (event) => {
-    const { charCode, target } = event;
-    if (charCode === 13) {
-      const newHashtags = [...hashtags];
-      if(target.value === '') return;
-      if (isDuplicateData(hashtags, target.value)) {
-        alert('이미 입력된 태그입니다!');
-      } else {
-        newHashtags.push(target.value);
+    const { keyCode, target } = event;
+    console.log(keyCode);
+    if (keyCode === 8) {
+      if(target.value === '#'){
+        event.preventDefault();
+        const newHashtags = [...hashtags];
+        newHashtags.splice(newHashtags.length - 1, 1);
         updateHashtags(newHashtags);
       }
-      target.value = '';
+    }
+    else if (keyCode === 13) {
+      const newHashtags = [...hashtags];
+      if(target.value === '#') return;
+      const value = target.value.slice(1);
+      if (isDuplicateData(hashtags, value)) {
+        alert('이미 입력된 태그입니다!');
+      } else {
+        newHashtags.push(value);
+        updateHashtags(newHashtags);
+      }
+      target.value = '#';
     }
   };
 
   return (<Grid container item>
-            <Hashtags hashtags={hashtags} updateHashtags={updateHashtags}/>
-            <TextField onKeyPress={handleHashtagInput}/>
+            <Grid container direction='row' spacing={1}>
+              {hashtags.length > 0 
+                ? (<Grid item>
+                    <Hashtags hashtags={hashtags} updateHashtags={updateHashtags}/>
+                  </Grid>)
+                : null}
+              <Grid item>
+                <StyledInput type='text' defaultValue='#' onKeyDown={handleHashtagInput} color='primary'/>
+              </Grid>
+            </Grid>
           </Grid>);
 }
 
--- src/Organisms/HashtagRecommender.js
+++ src/Organisms/HashtagRecommender.js
@@ -5,21 +5,32 @@ import {
 } from '@material-ui/core';
 import { isDuplicateData } from '../utils';
 
-const HashtagRecommender = ({ recommendedHashtags, hashtags, updateHashtags }) => (<Grid>
-    이런 해시태그는 어때요?
-    {recommendedHashtags.map((item) => (
-    <Chip key={item.key}
-      label={item.name}
-      onClick={() => {
-        const newHashtags = [...hashtags];
-        if(isDuplicateData(hashtags, item.name)){
-          alert('이미 입력된 태그입니다!');
-          return;
-        }
-    newHashtags.push(item.name);
+const HashtagRecommender = ({ recommendedHashtags, hashtags, updateHashtags }) => {
+  const handleChipClick = (value) => {
+    const newHashtags = [...hashtags];
+    if(isDuplicateData(hashtags, value)){
+      alert('이미 입력된 태그입니다!');
+      return;
+    }
+    newHashtags.push(value);
     updateHashtags(newHashtags);
-    }}
-    />))}
+  }
+
+  return (<Grid>
+    <Grid container direction='row' alignItems='center' spacing={1}>
+      <strong style={{ color: '#828282' }}>이런 해시태그는 어때요?</strong>
+      {recommendedHashtags.map((item, index) => (
+      <Grid item key={item.key}>
+        <Chip
+          label={`#${item.name}`}
+          color='secondary'
+          style={{ cursor: 'pointer' }}
+          onClick={() => { handleChipClick(item.name); }}
+        />
+      </Grid>
+      ))}
+    </Grid>
   </Grid>);
+};
 
 export default HashtagRecommender;
