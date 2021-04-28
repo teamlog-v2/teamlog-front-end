482910b50861c3903c6bca3834cd8b09c9857214
jduckling1024 <58102502+jduckling1024@users.noreply.github.com>
2021-04-28 15:47:24 +0900

    [수정] eslintrc.js 파일 수정

---
+++ src/file/filetest.js
@@ -0,0 +1,12 @@
+import { Box } from '@material-ui/core';
+import React from 'react';
+
+const FileTest = () => {
+    return(
+        <>
+            <Box />
+        </>
+    );
+}
+
+export default FileTest;
--- src/post-management/postlist.js
+++ src/post-management/postlist.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-
 import { Post } from './post';
 import data from './datalist';
 
