33958608ed68f475473950119316cde3946a40a6
hamchu <64580070+hamchu@users.noreply.github.com>
2021-05-02 21:21:34 +0900

    [추가] MyPage 컴포넌트

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -3,9 +3,12 @@ const headersData = { 'Content-Type': 'application/json' };
 const createUser = (data) => fetch('/api/users', {
     method: 'Post',
     body: JSON.stringify(data),
-    headers: headersData
-});
+    headers: headersData,
+  });
 
-export {
-    createUser
-};
+const getUser = (id) => fetch(`/api/users/${id}`, {
+    method: 'Get',
+    headers: headersData,
+  });
+
+export { createUser, getUser };
