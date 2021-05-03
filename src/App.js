a3d10f03a763a7b707fdc4c6da34f71fbaea02cc
eojin-dev <eojin-dev@naver.com>
2021-05-03 23:29:04 +0900

    임시 라우터 추가

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import ProjectListContainer from './project/ProjectListContainer';
 import Project from './project-management/project';
 import MyPage from './user/MyPage';
 import { ErrorProvider } from './contexts/error';
+import PostFormPage from './pages/PostFormPage';
 import ErrorPage from './pages/ErrorPage';
 import MapPage from './map/MapPage';
+import SignUp from './user/SignUp';
 
 const App = () => {
   return (
@@ -17,6 +19,8 @@ const App = () => {
           <Route path="/project" component={ProjectListContainer} />
           <Route exact path="/projects/:id" component={Project} />
           <Route exact path="/users/:userId" component={MyPage} />
+          <Route exact path="/new" component={PostFormPage} />
+          <Route exact path="/signup" component={SignUp} />
           <Route>
             <ErrorPage error="404" />
           </Route>
