078d888b731f6af80a1889a7706626530a8a1af2
eojin-dev <eojin-dev@naver.com>
2021-05-21 23:14:19 +0900

    [수정] 앱바 상단 고정

--- src/AppBar.js
+++ src/AppBar.js
@@ -5,26 +5,57 @@ import {
   Menu,
   MenuItem,
   MenuList,
+  Slide,
+  useScrollTrigger,
 } from '@material-ui/core';
 import { ArrowDropDown } from '@material-ui/icons';
 import React, { useContext, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import AuthContext, { setAccessToken } from './contexts/auth';
 
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined, threshold: 0 });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
 const Div = ({ children }) => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        margin: '8px 8px 0px 8px',
-        backgroundColor: '#E0BAF7',
-        borderRadius: '0.25rem',
-      }}
-    >
-      {children}
-    </div>
+    <>
+      <HideOnScroll>
+        <div
+          style={{
+            zIndex: 999,
+            // position: 'sticky',
+            // top: '0px',
+            width: '100%',
+            position: 'fixed',
+          }}
+        >
+          <div
+            style={{
+              margin: '0.25rem',
+              display: 'flex',
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+              backgroundColor: '#E0BAF7',
+              borderRadius: '0.25rem',
+            }}
+          >
+            {children}
+          </div>
+        </div>
+      </HideOnScroll>
+      <div style={{ height: '40px' }} />
+    </>
   );
 };
 
@@ -59,66 +90,70 @@ export default function AppBar() {
 
   if (!id) {
     return (
-      <Div>
-        <Button
-          color="white"
-          onClick={() => {
-            history.push('/login');
-          }}
-        >
-          로그인
-        </Button>
-      </Div>
+      <>
+        <Div>
+          <Button
+            color="white"
+            onClick={() => {
+              history.push('/login');
+            }}
+          >
+            로그인
+          </Button>
+        </Div>
+      </>
     );
   }
 
   return (
-    <Div>
-      <Button onClick={handleClick}>
-        <Avatar className={classes.small} />
-        <ArrowDropDown />
-      </Button>
-      <Menu
-        keepMounted
-        anchorEl={anchorEl}
-        open={!!anchorEl}
-        onClose={handleClose}
-      >
-        <MenuItem
-          onClick={() => {
-            handleClose();
-            history.push(`/users/${id}`);
-          }}
+    <>
+      <Div>
+        <Button onClick={handleClick}>
+          <Avatar className={classes.small} />
+          <ArrowDropDown />
+        </Button>
+        <Menu
+          keepMounted
+          anchorEl={anchorEl}
+          open={!!anchorEl}
+          onClose={handleClose}
         >
-          마이페이지
-        </MenuItem>
-        <MenuItem
-          onClick={() => {
-            handleClose();
-            history.push('/create-project');
-          }}
-        >
-          프로젝트 생성
-        </MenuItem>
-        <MenuItem
-          onClick={() => {
-            handleClose();
-            history.push('/create-team');
-          }}
-        >
-          팀 생성
-        </MenuItem>
-        <MenuItem>승인 현황</MenuItem>
-        <MenuItem
-          style={{ color: 'red' }}
-          onClick={() => {
-            handleClose();
-            handleLogout();
-          }}
-        >
-          로그아웃
-        </MenuItem>
-      </Menu>
-    </Div>
+          <MenuItem
+            onClick={() => {
+              handleClose();
+              history.push(`/users/${id}`);
+            }}
+          >
+            마이페이지
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              handleClose();
+              history.push('/create-project');
+            }}
+          >
+            프로젝트 생성
+          </MenuItem>
+          <MenuItem
+            onClick={() => {
+              handleClose();
+              history.push('/create-team');
+            }}
+          >
+            팀 생성
+          </MenuItem>
+          <MenuItem>승인 현황</MenuItem>
+          <MenuItem
+            style={{ color: 'red' }}
+            onClick={() => {
+              handleClose();
+              handleLogout();
+            }}
+          >
+            로그아웃
+          </MenuItem>
+        </Menu>
+      </Div>
+    </>
   );
 }
