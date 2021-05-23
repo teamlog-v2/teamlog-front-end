9496b895a0f0e12f2ff899cedee976e7d4cb4168
jduckling1024 <58102502+jduckling1024@users.noreply.github.com>
2021-05-23 23:48:50 +0900

    [추가] 애플리케이션 다운로드

---
+++ public/service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'teamlog-cache-v1';
+
+const FILES_TO_CACHE = [
+  'offline.html',
+];
+
+self.addEventListener('install', (evt) => {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('install');
+            return cache.addAll(FILES_TO_CACHE);
+        }),
+    );
+});
+
+self.addEventListener('activate', (evt) => {
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        }),
+    );
+});
+
+self.addEventListener('fetch', (evt) => {
+    // caches.open(CACHE_NAME).then((cache) => {
+    //     return cache.match(evt.request)
+    //     .then((response) => {
+    //         return response || fetch(evt.request);
+    //     });
+    // });
+
+    if (evt.request.mode !== 'navigate') {
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+        .catch(() => {
+            return caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.match('offline.html');
+            });
+        }),
+    );
+});
