2780c6714d7c1cd4ceaed487e8853e3d535fdcc9
eojin-dev <eojin-dev@naver.com>
2021-05-25 16:50:02 +0900

    _ 메인페이지 뉴스피드 작업 중 2

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -32,6 +32,9 @@ export default function HomePage() {
   const [followingPosts, setFollowingPosts] = useState([]);
   const followingPostsCounter = useRef(0);
 
+  // taskWrapper와 postWrapper들이 들어간다.
+  const [units, setUnits] = useState(null);
+
   useEffect(() => {
     // 초대장
     fetch('/api/users/project-invitation')
@@ -75,16 +78,23 @@ export default function HomePage() {
     // 프로젝트 단위로 받는데,
     // 최신 (5일 이내? 기준시간값을 변경하기 쉽게 구현하세요)측에 속하는 것만 가지고 나머지는 날린다. (이러고 남은게 없으면 패스~)
     // 정렬 후 포장해서 저장한다. wrapper.isTask = true
-    belongingProjects.forEach(({ id }) => {
-      fetch(`/api/projects/${id}/tasks`)
+    belongingProjects.forEach((project) => {
+      fetch(`/api/projects/${project.id}/tasks`)
         .then((res) => res.json())
         .then((res) => {
           console.log(res);
           const filteredTasks = res; // TODO: 최신 항목 분리
 
           if (filteredTasks.length > 0) {
-            filteredTasks.sort(); // TODO: 최신 순 정렬
-            const taskWrapper = { type: 'TASK', tasks: filteredTasks, projectId: id, wrapperTime: filteredTasks[0].updateTime }; // TODO: 기준시각 속성 변경
+            // TODO: 최신 순 정렬
+            // filteredTasks.sort();
+            // TODO: wrapperTime 변경
+            const taskWrapper = {
+              type: 'TASKS',
+              tasks: filteredTasks,
+              project,
+              wrapperTime: filteredTasks[0].updateTime,
+            };
             setTaskWrappers((prev) => [...prev, taskWrapper]);
           }
           tasksCounter.current += 1;
@@ -100,6 +110,7 @@ export default function HomePage() {
         .then((res) => res.json())
         .then((res) => {
           console.log(res);
+          // TODO : 최신 항목 분리
           setBelongingPosts((prev) => [...prev, ...res.content]);
           belongingPostsCounter.current += 1;
         });
@@ -120,6 +131,7 @@ export default function HomePage() {
         .then((res) => res.json())
         .then((res) => {
           console.log(res);
+          // TODO : 최신 항목 분리
           setFollowingPosts((prev) => [...prev, ...res.content]);
           followingPostsCounter.current += 1;
         });
@@ -139,15 +151,31 @@ export default function HomePage() {
     }
 
     console.log(invitations);
-
-    //
-
     console.log(taskWrappers);
     console.log(belongingPosts);
     console.log(followingPosts);
 
-    // 여기서 포스트 및 태스크 요소의 정렬이 필요함. 중복 포스트도 날려야함
+    // 중복 포스트를 날리고, Wrapping 하기
+    const postWrappers = [];
+    const checker = {};
+    [...belongingPosts, ...followingPosts].forEach((post) => {
+      if (checker[post.id]) {
+        return;
+      }
+      checker[post.id] = true;
+      postWrappers.push({
+        type: 'POST',
+        post,
+        project: post.project,
+        wrapperTime: post.writeTime,
+      });
+    });
 
+    // wrapper 통합하기
+    const wrappers = [...taskWrappers, ...postWrappers];
+    // TODO: wrapperTime으로 wrappers를 정렬
+
+    setUnits(wrappers);
     setIsLoaded(true);
   }, [invitations, taskWrappers, belongingPosts, followingPosts]);
 
@@ -159,26 +187,25 @@ export default function HomePage() {
   return (
     <>
       <h1>최신 뉴스 피드</h1>
+
       <Divider />
-      <h1>내가 받은 프로젝트 초대장</h1>
       {invitations.map((unit) => (
         <div key={unit.id}>{unit.projectName}</div>
       ))}
+
       <Divider />
-      {/* <h1>내가 속한 프로젝트의 최근 테스크 업데이트 소식</h1>
-      {tasks.map((task) => (
-        <div key={task.id}>{task.taskName}</div>
-      ))} */}
-      <Divider />
-      <h1>내가 속한 프로젝트의 최근 게시물 업데이트 소식</h1>
-      {belongingPosts.map((post) => (
-        <div key={post.id}>{post.contents}</div>
-      ))}
-      <Divider />
-      <h1>내가 팔로우하는 프로젝트의 최근 게시물 업데이트 소식</h1>
-      {followingPosts.map((post) => (
-        <div key={post.id}>{post.contents}</div>
-      ))}
+      {units.map((wrapper) => {
+        switch (wrapper.type) {
+          case 'POST':
+            return <div key={`POST_${wrapper.post.id}`}>여기에 포스트</div>;
+          case 'TASKS':
+            return (
+              <div key={`TASKS_${wrapper.project.id}`}>여기에 테스크스</div>
+            );
+          default:
+            return null;
+        }
+      })}
       <Divider />
     </>
   );
