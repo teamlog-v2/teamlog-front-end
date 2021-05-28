c123ecfbb4dad849d303ed3b630a3893fbf266fa
HyeonDoonge <migu554@naver.com>
2021-05-29 00:33:34 +0900

    [수정] 첨부파일 리스트 컴포넌트

--- src/post-management/fileList.js
+++ src/post-management/fileList.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Attachment from '@material-ui/icons/Attachment';
-import { makeStyles } from '@material-ui/core';
+import { Badge, Chip, makeStyles, Tooltip } from '@material-ui/core';
 
 const useStyles = makeStyles(() => ({
   root: {
-    '& > *': {
+    fontSize: 14,
+    '& .file ': {
       padding: '1%',
-      // borderTop: '1px solid #eee',
-    },
-    '& > :last-child': {
-      // borderBottom: '1px solid #eee',
+      marginLeft: 10,
+      '& :hover': {
+        opacity: 0.4,
+      },
     },
   },
 }));
@@ -23,24 +24,27 @@ const FileList = ({ files }) => {
     <Grid className={classes.root}>
       {
         files && (
-        <Grid container item alignItems="center">
-          <Attachment style={{ height: '20px' }} />
-          {files[0].fileName}외 {files.length - 1}개의 첨부파일
+        <Grid
+          container
+          item
+          alignItems="center"
+          onClick={() => { setOpen((isOpen) => !isOpen); }}
+        >
+          <Tooltip title={`${files[0].fileName} ${files.length > 1 ? `외 ${files.length - 1}개` : ''}`}>
+            <Chip label="첨부" color="primary" size="small" icon={<Attachment fontSize="small" />} clickable />
+          </Tooltip>
         </Grid>
         )
       }
       { open && (
       files.map((file) => (
-        <Grid container alignItems="center">
-          <div>
-            <Attachment style={{ height: '15px' }} />
-            <a
-              href={file.fileDownloadUri}
-              style={{ color: 'black', textDecoration: 'none' }}
-            >
-              {file.fileName}
-            </a>
-          </div>
+        <Grid className="file" container alignItems="center">
+          <a
+            href={file.fileDownloadUri}
+            style={{ color: 'black', textDecoration: 'none' }}
+          >
+            {file.fileName}
+          </a>
         </Grid>
       )))}
     </Grid>
