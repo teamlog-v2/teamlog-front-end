3ee9787b03e15c700013f4f2b67b1f74db5f794d
JIN <jduck1024@naver.com>
2023-12-10 15:05:54 +0900

    REFACTOR-2 service-worker 비활성화

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,53 +1,47 @@
-/* pusher test */
-importScripts("https://js.pusher.com/beams/service-worker.js");
-/* pusher test */
+// /* pusher test */
+// importScripts("https://js.pusher.com/beams/service-worker.js");
+// /* pusher test */
 
-const CACHE_NAME = 'teamlog-cache-v2';
+// const CACHE_NAME = 'teamlog-cache-v2';
 
-const FILES_TO_CACHE = [
-  'offline.html',
-];
+// const FILES_TO_CACHE = [
+//     'offline.html',
+// ];
 
-self.addEventListener('install', (evt) => {
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log('install');
-            return cache.addAll(FILES_TO_CACHE);
-        }),
-    );
-});
+// self.addEventListener('install', (evt) => {
+//     evt.waitUntil(
+//         caches.open(CACHE_NAME).then((cache) => {
+//             console.log('install');
+//             return cache.addAll(FILES_TO_CACHE);
+//         }),
+//     );
+// });
 
-self.addEventListener('activate', (evt) => {
-    evt.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(keyList.map((key) => {
-                if (key !== CACHE_NAME) {
-                    console.log('Removing old cache', key);
-                    return caches.delete(key);
-                }
-            }));
-        }),
-    );
-});
+// self.addEventListener('activate', (evt) => {
+//     evt.waitUntil(
+//         caches.keys().then((keyList) => Promise.all(keyList.map((key) => {
+//             if (key !== CACHE_NAME) {
+//                 console.log('Removing old cache', key);
+//                 return caches.delete(key);
+//             }
+//         }))),
+//     );
+// });
 
-self.addEventListener('fetch', (evt) => {
-    // caches.open(CACHE_NAME).then((cache) => {
-    //     return cache.match(evt.request)
-    //     .then((response) => {
-    //         return response || fetch(evt.request);
-    //     });
-    // });
+// self.addEventListener('fetch', (evt) => {
+//     // caches.open(CACHE_NAME).then((cache) => {
+//     //     return cache.match(evt.request)
+//     //     .then((response) => {
+//     //         return response || fetch(evt.request);
+//     //     });
+//     // });
 
-    if (evt.request.mode !== 'navigate') {
-        return;
-    }
-    evt.respondWith(
-        fetch(evt.request)
-        .catch(() => {
-            return caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.match('offline.html');
-            });
-        }),
-    );
-});
+//     if (evt.request.mode !== 'navigate') {
+//         return;
+//     }
+//     evt.respondWith(
+//         fetch(evt.request)
+//             .catch(() => caches.open(CACHE_NAME)
+//                 .then((cache) => cache.match('offline.html'))),
+//     );
+// });
